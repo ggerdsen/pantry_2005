75a4a75e76b8537ad4ce2e144eef10a20a9d4285
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-06-03 10:24:48 -0600

    created test_it_creates_pantry and associtated code

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,1 +1,12 @@
+class Pantry
 
+  attr_reader :stock
+  def initialize(info)
+    @stock = {}
+  end
+
+  def stock_check(ingredient)
+
+  end
+
+end
--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -7,6 +7,8 @@ class IngredientTest < Minitest::Test
 
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @pantry = Pantry.new
   end
 
   def test_it_exists
@@ -19,6 +21,14 @@ class IngredientTest < Minitest::Test
 
   def test_it_has_a_unit
     assert_equal "oz", @ingredient1.unit
+  end
+
+  def test_it_has_calories
+    assert_equal 50, @ingredient1.calories
+  end
+
+  def test_it_creates_pantry
+    assert_instance_of Pantry, @pantry
   end
 
 end
