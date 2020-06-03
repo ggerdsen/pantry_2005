b263df7c75d61fb96cc44536af0d4a301d4ad728
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-06-03 11:49:26 -0600

    create test_it_returns_calories test

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -6,4 +6,24 @@ require './lib/recipe'
 require './lib/cook_book'
 require 'pry'
 
-class RecipeTest < Minitest::Test
+class CookBookTest < Minitest::Test
+
+  def setup
+    @pantry = Pantry.new
+    @cookbook = CookBook.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe1.add_ingredient(ingredient1, 2)
+    @recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe2 = Recipe.new("Cheese Burger")
+  end
+
+  def test_it_returns_calories
+
+  end
+
+
+end
